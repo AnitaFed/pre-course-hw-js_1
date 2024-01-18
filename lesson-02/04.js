6e73828102332e89590a95b2c0db99bf61c4f27e
AnitaFed <anita.fed2016@gmail.com>
2024-01-19 01:41:24 +0200

    t0004

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,8 +1,9 @@
-const giveMeRightCondition = (value1, value2, maximum) => {
+let giveMeRightCondition = (value1, value2, maximum) => {
     if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
+
 giveMeRightCondition(100, 100, 200);
