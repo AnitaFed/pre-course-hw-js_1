c51486597c8040d7a17a30cd17afa9ba70bd10c1
AnitaFed <anita.fed2016@gmail.com>
2024-01-19 00:49:14 +0200

    test_03_003

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,2 +1,8 @@
-
-giveMeRightNumbers(200, 100, 200)
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(200, 100, 150)
