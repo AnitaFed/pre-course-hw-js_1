9f3936b10561a82f12ead3e89323ebebdd99a2ca
AnitaFed <anita.fed2016@gmail.com>
2024-01-22 21:05:52 +0200

    test00055

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -2,13 +2,8 @@ let passport = {
     name: "Petr",
     surname: "Petrov",
 };
-
-// Создаем копию объекта passport
-let myPassport = Object.assign({}, passport);
-
-// Меняем имя в копии объекта
-myPassport.name = "Ivan";
-
-// Выводим значения имен в консоли
-console.log("Оригинальный passport:", passport);
-console.log("Мой passport:", myPassport);
+let newPassport = {
+    ...passport, name: "Ivan"
+}
+console.log(passport.name);
+console.log(newPassport.name);
